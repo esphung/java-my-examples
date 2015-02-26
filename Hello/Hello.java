20d927662d0fc7e682d0ca790c56c5e800abc08a
Eric Phung <esphung@iupui.edu>
2015-02-26 15:41:31 -0500

    Templates for development in Java

---
+++ Hello/Hello.java
@@ -0,0 +1,13 @@
+/*
+* @File:		Hello World in Java
+* @Author:	Eric Phung
+* @Date:		2015-02-26 15:21:03
+* @Last Modified by:   Eric Phung
+* @Last Modified time: 2015-02-26 15:35:51
+*/
+public class Hello {
+
+    public static void main(String[] args){
+	System.out.println("Hello, world!");
+    } // end main
+} // end class def
